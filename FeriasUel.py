1c22ecdd0d5e5886e4480538d69f4c1455bb267a
Arthur Pereira <arthurap2001@gmail.com>
2023-10-27 13:00:07 -0300

    Atualização do bot para nova API

--- FeriasUel.py
+++ FeriasUel.py
@@ -1,42 +1,47 @@
-### Bot V0.2
+### Bot V0.3
 
 import datetime 
 import tweepy
 
-# Authenticate to Twitter
+# Autenticação do Bot
 
-# Keys and secrets must be hidden
+# Keys e Secrets devem estão "escondidas"
 
-auth = tweepy.OAuthHandler('', '')
-auth.set_access_token('', '')
+key = ''
+secret = ''
 
-api = tweepy.API(auth)
+bearer = ''
 
-# Determine the actual date and how many days are left
+consumer_key = ''
+consumer_secret = ''
 
-ferias = datetime.date(2022, 12, 10)
+api = tweepy.Client(bearer,key,secret,consumer_key,consumer_secret)
+
+# Data das datas
+
+ferias = datetime.date(2023, 12, 16)
 agora = datetime.date.today()
 
 fdias =(ferias - agora).days
 
-# Find wich day of the week is the date
-agora.weekday()
+# Acha o dia da semana
+#agora.weekday()
 
 # Ifs to determine the days
 
 if fdias != 1:
-
-
-  if agora.weekday() == 0:
-    api.update_status_with_media("Faltam " + str(fdias) + " dias para as férias da UEL!\n", '/home/leninjitsu/Bot/eu odeio segunda.jpg')
-  elif agora.weekday() == 4:
-    api.update_status_with_media("Faltam " + str(fdias) + " dias para as férias da UEL\nE hoje é sexta-feira!", 'Bot_Ferias_UEL/shrek-sextafeira.gif')
-  else:
-    api.update_status("Faltam " + str(fdias) + " dias para as férias da UEL!")
-
+  api.create_tweet(text="Faltam " + str(fdias) + " dias para as férias da UEL!")
 elif fdias == 1:
-  api.update_status("Ultimo dia de aula da UEL!")
-
+  api.create_tweet(text="Ultimo dia de aula da UEL!")
 else:
-  api.update_status("Férias da UEL!")
+  api.create_tweet(text="Férias da UEL!!!")
+
+# parte antiga do código
+
+#   if agora.weekday() == 0:
+#     api.update_status_with_media("Faltam " + str(fdias) + " dias para as férias da UEL!\n", '/home/leninjitsu/Bot/eu odeio segunda.jpg')
+#   elif agora.weekday() == 4:
+#     api.update_status_with_media("Faltam " + str(fdias) + " dias para as férias da UEL\nE hoje é sexta-feira!", 'Bot_Ferias_UEL/shrek-sextafeira.gif')
+#   else:
+#     api.update_status("Faltam " + str(fdias) + " dias para as férias da UEL!")
 
